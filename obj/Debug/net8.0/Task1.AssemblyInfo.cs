1a03177bb7de799af5556429e06493af165ef4a4
shaimaa-alkharraz <shaimaa.kh04@gmail.com>
2025-07-19 14:04:38 -0700

    Added Entity Framework and database settings

--- obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e19bb1cb9c48aa99298796543760f55e13ef4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
